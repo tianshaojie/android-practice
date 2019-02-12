ab5a0877de36bedcdfc2894be3d9ef2811a70bc5
tianshaojie <tianshaojie@foundersc.com>
2019-02-12 12:25:50 +0800

    init app_status

--- app/src/main/java/cn/skyui/practice/activity/MainActivity.java
+++ app/src/main/java/cn/skyui/practice/activity/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        BaseActivity.APP_STATUS = BaseActivity.APP_STATUS_NORMAL;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
