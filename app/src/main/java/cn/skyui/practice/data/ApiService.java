5db44eb52476a71fcc9acbefb3d2918cd216a0c0
tianshaojie <tianshaojie@foundersc.com>
2019-03-11 18:05:56 +0800

    remove fd

--- app/src/main/java/cn/skyui/practice/data/ApiService.java
+++ app/src/main/java/cn/skyui/practice/data/ApiService.java
@@ -6,10 +6,4 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
-    static final String BASE_URL = "https://app.foundersc.com/";
-
-    // https://app.foundersc.com/quote/search/m?text=600&count=40
-    @GET("quote/search/m")
-    Observable<FounderHttpResponse<String>> search(@Query("text") String text, @Query("count") int count);
-
 }
